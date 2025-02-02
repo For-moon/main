7977975d6a074ea636a23f14d4c0ea839af8decd
For-moon <yuki3d1415@gmail.com>
2025-02-02 19:28:26 +0700

    Add files via upload

---
+++ 2025/01-29/data.js
@@ -0,0 +1,12 @@
+const data_name = [{
+	ma_ten: "hang",
+	name: "Hằng",
+	message: "Hello, I'm Hằng",
+	img: "https://vcdn1-giaitri.vnecdn.net/2014/11/12/12-1415753927.jpg?w=1200&h=0&q=100&dpr=1&fit=crop&s=eeNdzX-KP7izXlmIm_xMCg"
+},
+	{
+		ma_ten: "hien",
+		name: "Hiền",
+		message: "Hello, I'm Hiền",
+		img: "https://vcdn1-giaitri.vnecdn.net/2014/11/12/12-1415753927.jpg?w=1200&h=0&q=100&dpr=1&fit=crop&s=eeNdzX-KP7izXlmIm_xMCg"
+	}]
